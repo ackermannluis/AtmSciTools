60e864611d91676d76d701fac3e6b5193ee74c3e
Luis Ackermann <55727347+ackermannluis@users.noreply.github.com>
2022-04-07 11:45:53 +1000

    initial script upload

---
+++ atmscitools/__init__.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from pathlib import Path
+import warnings
+import sys
+
+warnings.filterwarnings("ignore")
+
+__version__ = '1.0.0'
+
+# Get path to mymodule
+AtmSciTools_path = str(Path(__file__).parent)
+sys.path.append(AtmSciTools_path)
+
+# import subpackages
+try:
+    import cdsapi
+except:
+    print('no cdsapi')
+
+try:
+    import IMProToo_mod
+except:
+    print('no IMProToo_mod')
+
+try:
+    import SkewT_V2 as SkewT
+except:
+    print('no SkewT_V2')
+
