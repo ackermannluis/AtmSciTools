1c0a0fc667749467606bfd068a9c09e0285994f5
ackermannluis <ackermannluis@gmail.com>
2022-04-07 12:15:08 +1000

    updated setup.py to correct wrong description

--- setup.py
+++ setup.py
@@ -16,26 +16,26 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="pyhail",
-    version="2.0.0",
-    description="Python Hail Weather Radar Retrievals",
+    name="atmscitools",
+    version="1.0.0",
+    description="Atmospheric Science Tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joshuass/PyHail",
-    author="Joshua Soderholm",
-    author_email="joshua.soderholm@bom.gov.au",  # Optional
+    url="https://github.com/ackermannluis/AtmSciTools",
+    author="Luis Ackermann",
+    author_email="ackermann.luis@gmail.com",  # Optional
     # For a list of valid classifiers, see https://pypi.org/classifiers/
     classifiers=[  # Optional
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Atmospheric Science",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache 2.0",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    keywords="radar weather meteorology calibration",
+    keywords="weather meteorology instrumentation",
     packages=find_packages(exclude=["contrib", "docs", "tests", "notebooks"]),
-    install_requires=["arm_pyart", "numpy", "netCDF4"],
-    project_urls={"Source": "https://github.com/joshuass/PyHail/",},
+    install_requires=["numpy", "netCDF4", "scipy", "paramiko", "matplotlib"],
+    project_urls={"Source": "https://github.com/ackermannluis/AtmSciTools/",},
 )
